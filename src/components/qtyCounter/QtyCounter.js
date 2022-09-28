20f93a71d08ec5afe9660ef02db73d348d66e024
Butovets Pavlo <p.butovets@gmail.com>
2022-09-29 01:06:26 +0300

    min qty = 1 fixed

--- src/components/qtyCounter/QtyCounter.js
+++ src/components/qtyCounter/QtyCounter.js
@@ -4,19 +4,25 @@ import plus from '../../resourses/icons/plus.svg';
 import minus from '../../resourses/icons/minus.svg';
 import './qtyCounter.scss';
 
-const QtyCounter = (props) => {
+const QtyCounter = () => {
 
     const [quantity, setQuantity] = useState(1);
 
-    const changeQuantity = (num) => {
-        setQuantity(quantity => quantity + num)
+    const incraeseQty = () => {
+        setQuantity(quantity => quantity + 1)
+    }
+
+    const decraeseQty = () => {
+        if (quantity > 1) {
+            setQuantity(quantity => quantity - 1)
+        }
     }
 
     return (
         <div className="qtyCounter">
-            <img src={minus} alt="minus" onClick={() => changeQuantity(-1)} />
+            <img src={minus} alt="minus" onClick={() => decraeseQty()} />
             <div className="qtyCounter-num">{quantity}</div>
-            <img src={plus} alt="plus" onClick={() => changeQuantity(1)} />
+            <img src={plus} alt="plus" onClick={() => incraeseQty()} />
         </div>
     )
 }
