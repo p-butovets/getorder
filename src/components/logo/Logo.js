da97589a45300c6f5ab18620c187202d695ecfca
Butovets Pavlo <p.butovets@gmail.com>
2022-10-06 18:03:23 +0300

    click on logo navigates to home page

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import logo from '../../resourses/logo/logo.png';
 import './logo.scss';
 
 const Logo = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div className="logo">
+        <div className="logo"
+            onClick={() => navigate('/')}>
             <img className="logo_image" src={logo} alt="logo" />
         </div>
     )
