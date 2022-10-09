99fa0d0986a2632d42cd3b349ac6d2f85ac887c8
Butovets Pavlo <p.butovets@gmail.com>
2022-10-09 13:46:00 +0300

    add phone button

--- src/components/sellPointHeading/SellPointHeading.js
+++ src/components/sellPointHeading/SellPointHeading.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import clock from '../../resourses/icons/clock.svg';
 import like from '../../resourses/icons/like.svg';
+import phone from '../../resourses/icons/phone.svg';
 import './sellPointHeading.scss';
 
 // как буд-то запрос на список ресторанов
@@ -24,21 +25,26 @@ const SellPointHeading = (props) => {
 
     return (
         <div className="restaurant-heading">
-            <div className="restaurant-heading__name">
-                {name}
+            <div className="restaurant-heading__left">
+                <div className="restaurant-heading__name">
+                    {name}
+                </div>
+                <div className="restaurant-heading__description">
+                    {description}
+                </div>
+                <div className="restaurant-heading__info">
+                    <span className="info-item">
+                        <img src={clock} alt="open time" />
+                        11:00 - 20:45
+                    </span>
+                    <span className="info-item">
+                        <img src={like} alt="rating" />
+                        4,5
+                    </span>
+                </div>
             </div>
-            <div className="restaurant-heading__description">
-                {description}
-            </div>
-            <div className="restaurant-heading__info">
-                <span className="info-item">
-                    <img src={clock} alt="open time" />
-                    11:00 - 20:45
-                </span>
-                <span className="info-item">
-                    <img src={like} alt="rating" />
-                    4,5
-                </span>
+            <div className="restaurant-heading__right">
+                <img src={phone} alt="phone" />
             </div>
         </div>
     )
